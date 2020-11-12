20d412a8d36bc4e644f3361612707c2ce99e5f83
Mandy Alber <mandy.alber@gmail.com>
2020-11-12 11:45:34 -0800

    ready for deployment

--- seeds/seed.noteful_notes.sql
+++ seeds/seed.noteful_notes.sql
@@ -1,5 +1,13 @@
 INSERT INTO noteful_notes (name, modified, content, folder_id)
 VALUES
-('Dogs','2019-01-03T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 1)
+('Dogs','2019-01-03T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 1),
+('Cats','2019-01-03T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 2),
+('Birds','2019-11-03T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 3),
+('Monkeys','2017-01-04T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 1),
+('Giraffes','2019-10-03T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 2),
+('Rabbits','2019-01-03T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 3),
+('Snakes','2019-02-09T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 1),
+('Elephants','2018-02-03T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 2),
+('Lions','2020-01-03T00:00:00.000Z','Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. ', 3),
 ;
  
